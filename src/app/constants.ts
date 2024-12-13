b4d591a19e34cc12a86f70cc55379d2073ad6b4b
Arnay89 <115412898+Arnay89@users.noreply.github.com>
2024-12-13 13:42:44 +0000

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://ab7d67b0603c44fcd84f417f535ac140-159614303.eu-west-2.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://a0b9aca0759d949829786b0918f5de35-694953775.eu-west-2.elb.amazonaws.com/',
 };
